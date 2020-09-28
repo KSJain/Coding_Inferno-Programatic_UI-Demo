c7e32160d0dade9cf5c404a532be174ba285962e
Kartikeya Saxena Jain <kartikeya.saxena.jain@gmail.com>
2020-09-28 18:11:42 -0400

    CIBD V0.0.0 2020-Sep-28 - Programatic UI
    Initial setup with  Programatic  UI and No Storyboard

---
+++ Coding_Inferno-Programatic_UI-Demo/Screen Views/IntroViewController.swift
@@ -0,0 +1,19 @@
+//
+//  IntroViewController.swift
+//  Coding_Inferno-Programatic_UI-Demo
+//
+//  Created by Kartikeya Saxena Jain on 9/28/20.
+//
+
+import UIKit
+
+class IntroViewController: ViewController {
+    
+    override func viewDidLoad() {
+        configureView()
+    }
+    
+    private func configureView() {
+        view.backgroundColor = .red
+    }
+}
